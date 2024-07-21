1d7adb1390e55645449cdd4f83c431401dac7412
Sithu <cithukyaw@gmail.com>
2024-07-21 22:28:48 +0630

    Add fonts

--- src/components/FlashCard.tsx
+++ src/components/FlashCard.tsx
@@ -25,12 +25,12 @@ const FlashCard: FC = () => {
   return (
     <div className="flash-card text-center flex flex-col justify-between h-screen">
       <div className="h-full">
-        <h2 className="font-bold h-2/4">{letter.thai}</h2>
+        <h2 className="font-bold h-2/4 th-bold">{letter.thai}</h2>
         <div className="flex justify-center items-center h-1/4">
           { pronunciation
             ?
             <div className="pronunciation">
-              <div className="text-3xl font-bold py-3">{letter.burmese}</div>
+              <div className="text-3xl font-bold py-3 my-bold">{letter.burmese}</div>
               <div className="text-3xl font-bold py-3">{letter.english}</div>
             </div>
             :
